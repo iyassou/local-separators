b727af1b4b13445ddece77b9a79211b1cea43f03
Iyassou Shimels <s.iyassou@gmail.com>
2022-04-19 11:43:19 +0100

    src/pickle.py: factorised overwrite guard, added MajorOpenRoadNetworks and stackoverflow pickling functions

--- src/pickle.py
+++ src/pickle.py
@@ -59,84 +59,7 @@ def pickle_local_separators(G: nx.Graph, overwrite: bool=False):
     with open(graph_pickle, 'wb') as handle:
         pickle.dump(local_cutvertices, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-def pickle_dataset(dataset: str, overwrite: bool=False):
-    '''
-        This function calls pickle_local_separators for each dataset obtained
-        via a dataset interface.
-
-        Parameters
-        ----------
-        dataset: str
-            Name of the dataset interface to connect to.
-        overwrite: bool, default False
-
-        Raises
-        ------
-        AttributeError
-            If dataset isn't a correct dataset interface.
-        ValueError
-            If I can't code correctly.
-    '''
-    raise NotImplementedError('have you fixed wildcarding a dataset?')
-    ### Obtain the dataset interface.
-    P: Providence = Providence()
-    D: DatasetInterface = next(
-        filter(
-            lambda x: x.file == dataset,
-            P.interfaces
-        )
-    )
-    ### Wildcard to obtain all datasets.
-    datasets: List[Union[nx.Graph, GETITEM_RETURN_TYPE]] = D['*']
-    ### Process the datasets in the dataset interface.
-    # Define helper function that processes the potentially and
-    # likely recursive data type.
-    def _pickle_dataset(ds: Union[nx.Graph, GETITEM_RETURN_TYPE], overwrite: bool):
-        '''
-            Helper function that processes the recursive data structure
-            and pickles all encountered datasets.
-
-            Parameters
-            ----------
-            ds: Union[nx.Graph, GETITEM_RETURN_TYPE]
-                Dataset which potentially consists of multiple nested
-                graphs whose local cutvertices we need to pickle.
-            overwrite: bool
-                Whether or not we should overwrite a pre-existing pickle.
-            
-            Raises
-            ------
-            ValueError
-                If the currently processed graph's name attribute isn't
-                a Path object.
-        '''
-        if isinstance(ds, nx.Graph):
-            # We simply have a Graph, pickle its local separators.
-            print(f'Pickling {ds.name.name}...')
-            start = time.time()
-            pickle_local_separators(ds, overwrite)
-            end = time.time()
-            print(f'Done. Took {round(end - start, 1)} seconds.')
-        else:
-        # elif isinstance(ds, GETITEM_RETURN_TYPE):
-            # We have to iterate through the values.
-            v: Union[GETITEM_RETURN_TYPE, List[nx.Graph]]
-            for v in ds.values():
-                if isinstance(v, GETITEM_RETURN_TYPE):
-                    pickle_local_separators(v, overwrite)
-                elif isinstance(v, List[nx.Graph]):
-                    for G in v:
-                        pickle_local_separators(G, overwrite)
-                else:
-                    raise ValueError(f'v is of type {type(v).__name__}')
-        # else:
-        #     raise ValueError(f'ds is of type {type(ds).__name__}')
-    # Call the helper function on the datasets contained within.
-    dataset: Union[nx.Graph, GETITEM_RETURN_TYPE]
-    for dataset in datasets:
-        _pickle_dataset(dataset, overwrite)
-
-def __pickle_Network_Neural_MJS20(overwrite: bool=False):
+def _overwrite_guard(overwrite: bool):
     if overwrite:
         print('OVERWRITING PICKLED LOCAL CUTVERTICES!')
         print('Confirm? [y/n]')
@@ -149,6 +72,9 @@ def __pickle_Network_Neural_MJS20(overwrite: bool=False):
             elif ans.lower().startswith('n'):
                 print('Overwrite aborted!')
                 exit()
+
+def __pickle_Network_Neural_MJS20(overwrite: bool=False):
+    _overwrite_guard(overwrite)
     from .datasets import Network_Data_MJS20 as ND
     from .utils import seconds_to_string as sec2str
     import time
@@ -170,18 +96,7 @@ def __pickle_Network_Neural_MJS20(overwrite: bool=False):
     print('Total pickling time:', sec2str(dur, rounding=2))
 
 def __pickle_roadNetCA(overwrite: bool=False):
-    if overwrite:
-        print('OVERWRITING PICKLED LOCAL CUTVERTICES!')
-        print('Confirm? [y/n]')
-        while True:
-            print('>>> ', end='')
-            ans: str = input()
-            if ans.lower().startswith('y'):
-                print('Overwrite confirmed.')
-                break
-            elif ans.lower().startswith('n'):
-                print('Overwrite aborted!')
-                exit()
+    _overwrite_guard(overwrite)
     from .datasets import roadNetCA
     from .utils import seconds_to_string as sec2str
     import time
@@ -197,5 +112,32 @@ def __pickle_roadNetCA(overwrite: bool=False):
     end: float = time.perf_counter()
     print('Total pickling time:', sec2str(end - start, rounding=3))
 
+def __pickle_stackoverflow(overwrite: bool=False):
+    _overwrite_guard(overwrite)
+    from .datasets import stackoverflow
+    from .utils import seconds_to_string as sec2str
+    import time
+    # Run routine to pickle stackoverflow dataset.
+    G: nx.Graph = stackoverflow['stackoverflow']
+    print(f'[1/1] Pickling {G.name.name}...')
+    start: float = time.perf_counter()
+    pickle_local_separators(G, overwrite=overwrite)
+    end: float = time.perf_counter()
+    print('Total pickling time:', sec2str(end - start, rounding=3))
+
+def __pickle_MajorOpenRoadNetworks(overwrite: bool=False):
+    _overwrite_guard(overwrite)
+    from .datasets import MajorOpenRoadNetworks
+    from .utils import seconds_to_string as sec2str
+    import time
+    # Run routine to pickle Major_Road_Network_2018_Open_Roads.zip dataset.
+    G: nx.Graph = MajorOpenRoadNetworks['Major_Road_Network_2018_Open_Roads']
+    print(f'[1/1] Pickling {G.name.name}...')
+    start: float = time.perf_counter()
+    pickle_local_separators(G, overwrite=overwrite)
+    end: float = time.perf_counter()
+    print('Total pickling time:', sec2str(end - start, rounding=3))
+
 if __name__ == '__main__':
-    __pickle_roadNetCA()
+    overwrite: bool = False
+    __pickle_MajorOpenRoadNetworks(overwrite=overwrite)
