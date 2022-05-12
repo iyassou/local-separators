c2ddb8dd029c0fd27ea6893fb5a2d9fd8ed81add
Iyassou Shimels <s.iyassou@gmail.com>
2022-05-12 11:56:10 +0100

    src/datasets/Network_Data_MJS20.py: we reading rat brains now

--- src/datasets/Network_Data_MJS20.py
+++ src/datasets/Network_Data_MJS20.py
@@ -11,9 +11,7 @@ from typing import (
 import sys
 
 _DATASET_EXTENSIONS: Set[str] = {'.dat'}
-_PROBLEMATIC_DATASETS: Tuple[str] = tuple(
-    f'rat_brain_{i+1}.dat' for i in range(3)
-)
+_PROBLEMATIC_DATASETS: Tuple[str] = ()
 Network_Data_MJS20: DatasetInterface = DatasetInterface(
     __file__, _DATASET_EXTENSIONS, _PROBLEMATIC_DATASETS, _edgelist_file_to_networkx_graph
 )
